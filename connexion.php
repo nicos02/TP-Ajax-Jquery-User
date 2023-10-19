cf7e73a4acab0b3327fce95ddc0723856e4392e6
nicos02 <135131275+nicos02@users.noreply.github.com>
2023-10-19 23:33:09 +0200

    Update commentaire

--- connexion.php
+++ connexion.php
@@ -8,7 +8,6 @@ $password = ""; // Mot de passe pour se connecter à la base de données
 try {
     $bdd = new PDO("mysql:host=$servername;dbname=blogsjt", $username, $password);
     // Création d'un nouvel objet PDO pour établir une connexion à la base de données
-    // "mysql:host=$servername;dbname=blogsjt" spécifie le type de base de données (MySQL), le nom du serveur ($servername) et le nom de la base de données (blogsjt)
     $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     // Configuration de PDO pour qu'il lance une exception en cas d'erreur
 
